1a508b239de4f7b473ca3ff09c868cd0d95dba09
Nicholas Patano <nicholaspatano@gmail.com>
2022-03-31 19:38:57 -0400

    a

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from "./routes/posts.js";
 import userRouter from "./routes/user.js";
 import "dotenv/config";
 
+const app = express();
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
